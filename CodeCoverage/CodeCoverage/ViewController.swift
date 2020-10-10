45427598130c2adb2494c50f5621311bc5ed4c16
Carmen Salvador <carbucio@gmail.com>
2020-10-10 13:29:29 -0700

    Chapter Three - Measure Code Coverage and Add Tests

---
+++ CodeCoverage/CodeCoverage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeCoverage
+//
+//  Created by Carmen Salvador on 10/10/20.
+//  Copyright © 2020 Carmen Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodeCoverage/CodeCoverageTests/CoveredClassTests.swift
@@ -0,0 +1,40 @@
+//
+//  CoveredClassTests.swift
+//  CodeCoverageTests
+//
+//  Created by Carmen Salvador on 10/10/20.
+//  Copyright © 2020 Carmen Salvador. All rights reserved.
+//
+
+@testable import CodeCoverage
+import XCTest
+
+class CoveredClassTests: XCTestCase {
+    func test_max_with1And2_shouldReturn2() {
+        let result = CoveredClass.max(1, 2)
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_max_with3And2_shouldReturn3() {
+        let result = CoveredClass.max(3, 2)
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_commaSeparated_from2to4_ShouldReturn234SeparatedByCommas() {
+        let result = CoveredClass.commaSeparated(from: 2, to: 4)
+        XCTAssertEqual(result, "2,3,4")
+    }
+    
+    func test_commaSeparated_from2to2_shouldReturn2WithNoComma() {
+        let result = CoveredClass.commaSeparated(from: 2, to: 2)
+        XCTAssertEqual(result, "2")
+    }
+    
+    func test_area_withWidth7_shouldBe49() {
+        let sut = CoveredClass()
+        
+        sut.width = 7
+        
+        XCTAssertEqual(sut.area, 49)
+    }
+}
